8926f447a33cd846d9d607f7eac5d79163c92c49
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 00:38:44 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,34 +1,50 @@
 let db;
 
 function initDB() {
-  const request = indexedDB.open("NexvoltDB", 1);
+  const request = indexedDB.open("NexvoltDB", 2);
 
   request.onupgradeneeded = function (event) {
     db = event.target.result;
 
+    // CLIENTS
     if (!db.objectStoreNames.contains("clients")) {
-      db.createObjectStore("clients", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("clients", {
+        keyPath: "id",
+        autoIncrement: true
+      });
     }
 
+    // JOBS
     if (!db.objectStoreNames.contains("jobs")) {
-      db.createObjectStore("jobs", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("jobs", {
+        keyPath: "id",
+        autoIncrement: true
+      });
     }
 
+    // EXPENSES
     if (!db.objectStoreNames.contains("expenses")) {
-      db.createObjectStore("expenses", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("expenses", {
+        keyPath: "id",
+        autoIncrement: true
+      });
     }
 
+    // INVOICES
     if (!db.objectStoreNames.contains("invoices")) {
-      db.createObjectStore("invoices", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("invoices", {
+        keyPath: "id",
+        autoIncrement: true
+      });
     }
   };
 
   request.onsuccess = function (event) {
     db = event.target.result;
-    console.log("Nexvolt DB ready");
+    console.log("Nexvolt DB Ready");
   };
 
   request.onerror = function () {
-    console.error("DB failed to open");
+    console.error("Database failed");
   };
 }
