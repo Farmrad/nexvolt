d7cd17a24161627d8b9ad0bcb5fa1e7ab50a92a6
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 14:43:03 +0100

    Update expenses.js

--- modules/expenses.js
+++ modules/expenses.js
@@ -1,40 +1,23 @@
-function init_expenses(){
-renderExpenses();
-}
+function init_expenses(){ render(); }
 
 function addExpense(){
 
-const type = document.getElementById("type").value;
-const amount = document.getElementById("amount").value;
-
 DB.push("expenses",{
-id:"EX-"+Date.now(),
-type,
-amount: Number(amount),
-date: new Date().toLocaleDateString()
+type: type.value,
+amount: Number(amount.value)
 });
 
-renderExpenses();
+render();
 }
 
-function renderExpenses(){
+function render(){
 
 const data = DB.get("expenses");
 
-document.getElementById("list").innerHTML =
-data.map((e,i)=>`
-
+list.innerHTML = data.map(e=>`
 <div class="card">
 <h3>${e.type}</h3>
 <p>${e.amount} TND</p>
-<p>${e.date}</p>
-
-<button onclick="DB.delete('expenses',${i}); renderExpenses()">
-Delete
-</button>
-
 </div>
-
 `).join("");
-
 }
