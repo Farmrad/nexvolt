9b6ca4b6b7dc096b3bbb0d5bdcf1d36a14c4274f
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 01:02:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("nexvolt-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/db/db.js"
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
