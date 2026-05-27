b74eb16fc3c7a2cef9804a163a5ea1cbf50d884b
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 14:46:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("nexvolt-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js",
-        "/db/db.js"
-      ]);
-    })
-  );
+const CACHE = "erp-v1";
+
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE).then(c=>{
+return c.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/core/db.js",
+"/core/router.js"
+]);
+})
+);
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r || fetch(e.request))
+);
 });
