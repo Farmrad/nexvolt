2d117bc172eb972c1dce455b1c3af327e0d0ebaa
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 12:27:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,54 @@
+/* =========================
+   ELECTRICIAN OS PRO
+========================= */
+
 let db;
 
 /* INIT */
 function initApp(){
+
 initDB();
-loadAll();
+
+loadPage("dashboard");
+
+}
+
+/* LOAD PAGES */
+async function loadPage(page){
+
+const content =
+document.getElementById("content");
+
+const res =
+await fetch(`pages/${page}.html`);
+
+const html =
+await res.text();
+
+content.innerHTML = html;
+
+/* LOAD PAGE DATA */
+if(page==="dashboard"){
+loadDashboard();
 drawChart();
 }
 
-/* NAV */
-function showPage(id){
-document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
-document.getElementById(id).classList.add("active");
+if(page==="jobs"){
+loadClientsIntoSelect();
 }
 
-/* THEME */
-function setTheme(mode){
-if(mode==="dark"){
-document.body.style.background="#0b1220";
-document.body.style.color="white";
-}
-if(mode==="light"){
-document.body.style.background="#f1f5f9";
-document.body.style.color="black";
-}
-}
-
-/* LOAD DATA */
-function loadAll(){
-loadDashboard();
 }
 
 /* DASHBOARD */
 function loadDashboard(){
 
-let tx=db.transaction(["jobs","expenses"],"readonly");
+document.getElementById("income").innerText =
+"5200 TND";
 
-let j=tx.objectStore("jobs").getAll();
-let e=tx.objectStore("expenses").getAll();
+document.getElementById("expense").innerText =
+"1800 TND";
 
-j.onsuccess=()=>{
-e.onsuccess=()=>{
+document.getElementById("profit").innerText =
+"3400 TND";
 
-let income=j.result.reduce((a,b)=>a+b.total,0);
-let expense=e.result.reduce((a,b)=>a+b.amount,0);
-
-document.getElementById("income").innerText=income;
-document.getElementById("expense").innerText=expense;
-document.getElementById("profit").innerText=income-expense;
-
-};
-};
-}
-
-/* CHART */
-function drawChart(){
-
-const ctx=document.getElementById("chart");
-
-new Chart(ctx,{
-type:"doughnut",
-data:{
-labels:["Income","Expenses","Profit"],
-datasets:[{
-data:[60,25,15],
-backgroundColor:["#22c55e","#ef4444","#3b82f6"]
-}]
-}
-});
 }
