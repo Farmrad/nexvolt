330fde63cc254d8f3d445e70bc99b8b8122015d5
Farmrad <166919506+Farmrad@users.noreply.github.com>
2026-05-27 14:29:20 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,24 +1,9 @@
 const DB = {
-
-get(key){
-return JSON.parse(localStorage.getItem(key)) || [];
-},
-
-set(key,value){
-localStorage.setItem(key, JSON.stringify(value));
-},
-
-push(key,item){
-let data = this.get(key);
-data.push(item);
-this.set(key,data);
-return item;
-},
-
-delete(key,index){
-let data = this.get(key);
-data.splice(index,1);
-this.set(key,data);
+get(k){ return JSON.parse(localStorage.getItem(k)) || []; },
+set(k,v){ localStorage.setItem(k, JSON.stringify(v)); },
+push(k,v){
+let d = DB.get(k);
+d.push(v);
+DB.set(k,d);
 }
-
 };
